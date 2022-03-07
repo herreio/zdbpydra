d63bf95f6989fc1c7074433fc40f3c8d7d50ce78
Donatus Herre <donatus.herre@slub-dresden.de>
2022-03-07 12:27:46 +0100

    Update methods of PicaParser

--- zdbpydra/docs.py
+++ zdbpydra/docs.py
@@ -8,6 +8,9 @@ https://zeitschriftendatenbank.github.io/pica3plus/ or
 https://zeitschriftendatenbank.de/erschliessung/zdb-format (both in german).
 """
 
+import types
+from . import utils
+
 
 class BaseParser:
 
@@ -272,13 +275,210 @@ class PicaParser(BaseParser):
     """
 
     def __init__(self, data):
+        self._delim = "|"
         super().__init__(data)
+
+    @staticmethod
+    def clean(value):
+        return utils.clean_blanks(value)
+
+    @staticmethod
+    def clean_title(value):
+        if type(value) == str:
+            value = value.replace("¬", "")
+            value = value.replace("@", "")
+            return PicaParser.clean(value)
+
+    def _field_value(self, name, clean=False):
+        value = self._field(name)
+        if isinstance(value, list) and len(value) > 0:
+            if isinstance(value[0], list) and len(value[0]) > 0:
+                if isinstance(value[0][0], list) and len(value[0][0]) > 0:
+                    if clean:
+                        return PicaParser.clean(value[0][0][0])
+                    return value[0][0][0]
+
+    def _subfields(self, name):
+        fields = self._field(name)
+        if isinstance(fields, list):
+            for field in fields:
+                for subfield in field:
+                    yield subfield
+
+    def _subfield_value(self, name, subname, unique=False, clean=False, joined=False):
+        subfields = self._subfields(name)
+        if isinstance(subfields, types.GeneratorType):
+            if not unique:
+                values = []
+            for subfield in subfields:
+                if subname in subfield:
+                    if unique:
+                        if clean:
+                            return PicaParser.clean(subfield[subname])
+                        return subfield[subname]
+                    else:
+                        values.append(subfield[subname] if not clean
+                                      else PicaParser.clean(subfield[subname]))
+        if not unique and len(values) > 0:
+            if joined:
+                return self._delim.join(values)
+            return values
+
+    @property
+    def bbg(self):
+        """
+        002@/0500 – Bibliographische Gattung/Status
+        """
+        return self._field_value("002@")
 
     @property
     def idn(self):
         """
         003@/0100 – Identifikationsnummer des Datensatzes (IDN)
         """
-        value = self._field("003@")
-        if value is not None:
-            return value[0][0][0]
+        return self._field_value("003@")
+
+    @property
+    def issn(self):
+        """
+        005A/2010 – International Standard Serial Number (ISSN)
+
+            $0  ISSN (mit Bindestrich)
+        """
+        return self._field_value("005A")
+
+    @property
+    def issn_l(self):
+        """
+        005A/2010 – International Standard Serial Number (ISSN)
+
+            $0  ISSN (mit Bindestrich)
+        """
+        return self._subfield_value("005A", "l", unique=True)
+
+    @property
+    def title(self):
+        """
+        021A/4000 – Haupttitel, Titelzusätze, Paralleltitel, Verantwortlichkeitsangabe
+
+            $a  Haupttitel
+        """
+        value = self._subfield_value("021A", "a", unique=True)
+        return self.clean_title(value)
+
+    @property
+    def title_supplement(self):
+        """
+        021A/4000 – Haupttitel, Titelzusätze, Paralleltitel, Verantwortlichkeitsangabe
+
+            $d  Titelzusatz
+        """
+        return self._subfield_value("021A", "d", clean=True, joined=True)
+
+    @property
+    def title_responsibility(self):
+        """
+        021A/4000 – Haupttitel, Titelzusätze, Paralleltitel, Verantwortlichkeitsangabe
+
+            $h  Verantwortlichkeitsangabe
+        """
+        return self._subfield_value("021A", "h", unique=True, clean=True)
+
+    @property
+    def publisher(self):
+        """
+        033A/4030 – Veröffentlichungsangabe
+
+            $n  Angabe des Verlages
+        """
+        return self._subfield_value("033A", "n", unique=True, clean=True)
+
+    @property
+    def publisher_place(self):
+        """
+        033A/4030 – Veröffentlichungsangabe
+
+            $p  Erster Erscheinungsort
+        """
+        return self._subfield_value("033A", "p", unique=True, clean=True)
+
+    @property
+    def parallel_type(self):
+        """
+        039D/4243 – Beziehung auf Manifestationsebene – außer Reproduktionen
+
+            $n  Materialart, zeitliche Gültigkeit der Beziehung
+        """
+        return self._subfield_value("039D", "n", clean=True, joined=True)
+
+    @property
+    def parallel_bbg(self):
+        """
+        039D/4243 – Beziehung auf Manifestationsebene – außer Reproduktionen
+
+            $g  Bibliographische Gattung/Status (undokumentiert)
+        """
+        return self._subfield_value("039D", "g", joined=True)
+
+    @property
+    def parallel_idn(self):
+        """
+        039D/4243 – Beziehung auf Manifestationsebene – außer Reproduktionen
+
+            $9  IDN des zu verknüpfenden Bezugswerkes
+        """
+        return self._subfield_value("039D", "9", joined=True)
+
+    @property
+    def parallel_issn(self):
+        """
+        039D/4243 – Beziehung auf Manifestationsebene – außer Reproduktionen
+
+            $X  ISSN
+            $I  ISSN (undokumentiert)
+        """
+        value = []
+        value_x = self._subfield_value("039D", "X")
+        if isinstance(value_x, list):
+            value.extend(value_x)
+        value_i = self._subfield_value("039D", "I")
+        if isinstance(value_i, list):
+            value.extend(value_i)
+        if len(value) > 0:
+            return self._delim.join(value)
+
+    @property
+    def access_source(self):
+        """
+        047V/4713 – Angaben zu Open Access, Lizenzen und Rechten
+
+            $b  Herkunft der Angabe
+        """
+        return self._subfield_value("047V", "b", unique=True)
+
+    @property
+    def access_rights(self):
+        """
+        047V/4713 – Angaben zu Open Access, Lizenzen und Rechten
+
+            $c  Benennung des Rechts (Code)
+        """
+        return self._subfield_value("047V", "c", unique=True)
+
+    @property
+    def access_status(self):
+        """
+        047V/4713 – Angaben zu Open Access, Lizenzen und Rechten
+
+            $c  Benennung des Rechts (Code)
+        """
+        return self._subfield_value("047V", "o", unique=True)
+
+    @property
+    def dewey(self):
+        """
+        045U/5080 – DDC-Sachgruppen der ZDB
+
+            $e  DDC-Sachgruppen der ZDB
+        """
+        return self._subfield_value("045U", "e", joined=True)
