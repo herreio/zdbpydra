18f83f69355a6f8e593aacc3908da9a4b9afb993
Donatus Herre <donatus.herre@slub-dresden.de>
2022-03-15 19:51:15 +0100

    Bump version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="zdbpydra",
-    version="0.1.0",
+    version="0.1.1",
     author="Donatus Herre",
     author_email="donatus.herre@slub-dresden.de",
     description="ZDB Hydra API Client",
